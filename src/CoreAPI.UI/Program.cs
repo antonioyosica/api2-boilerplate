caa8b63b806b9e1d9652a869f637df1a2ca65008
António Yosica <antonioyosica@gmail.com>
2023-09-25 14:38:56 +0100

    Base: structure defined and setted

---
+++ src/CoreAPI.UI/Program.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using CoreAPI.Domain.Services;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
+builder.Services.AddTransient<ICqrsService, CqrsService>();
+builder.Services.AddMediatR(msc => msc.RegisterServicesFromAssembly(typeof(CoreAPI.Domain.Commands.Account.CreateAccountCommandHandler).Assembly));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                        new HeaderApiVersionReader("x-api-version"),
+                                                        new MediaTypeApiVersionReader("x-api-version"));
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
